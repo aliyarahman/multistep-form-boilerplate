2b98061fe944457c1f38f86ad867bf474663045e
aliyarahman <aliya.rahman@yahoo.com>
2019-07-04 17:02:22 -0400

    First form integrated into stepper logic

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,13 +1,59 @@
 import React, {Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        const { values, handleChange } = this.props;
+
         return (
-            <div>
-                <h1>User Details</h1>
-            </div>
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter Your User Details" />
+                    <TextField
+                        hintText="Enter your first name"
+                        floatingLabelText="First name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                    <br />
+                    <TextField
+                        hintText="Enter your last name(s)"
+                        floatingLabelText="Last names"
+                        onChange={handleChange('lastNames')}
+                        defaultValue={values.lastNames}
+                    />
+                    <br />
+                    <TextField
+                        hintText="Enter your email"
+                        floatingLabelText="Email"
+                        onChange={handleChange('email')}
+                        defaultValue={values.email}
+                    />
+                    <br />
+                    <RaisedButton
+                        label="Continue"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue}
+                    />
+                </React.Fragment>
+            </MuiThemeProvider>
         );
     }    
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
 }
 
 export default FormUserDetails;
